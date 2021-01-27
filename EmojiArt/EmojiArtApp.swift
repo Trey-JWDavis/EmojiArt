5bcfbb4a6bb53b7f7f92ac75e8dde7a421060f1c
Trey Davis <trey@jwdavis.tech>
2021-01-27 11:41:48 -0500

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Trey Davis on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
